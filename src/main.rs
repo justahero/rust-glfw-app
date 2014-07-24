2d24b27593e7d710ee7dc102d5a3d0e1e07aa6e4
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2014-07-24 22:04:44 +0200

    Adds gl-rs dependency, clears window to color
    * clears background to a color
    * sets OpenGL version and a few hints

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-extern crate native;
-extern crate glfw;
-extern crate core;
+#![feature(globs)]
 
+extern crate core;
+extern crate gl;
+extern crate glfw;
+extern crate native;
+
+// use gl::types::*;
 use glfw::Context;
 
 #[start]
@@ -11,6 +15,12 @@ fn start(argc: int, argv: *const *const u8) -> int {
 
 fn main() {
     let glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+
+    // Select OpenGL mode
+    glfw.window_hint(glfw::ContextVersion(3, 2));
+    glfw.window_hint(glfw::OpenglForwardCompat(true));
+    glfw.window_hint(glfw::OpenglProfile(glfw::OpenGlCoreProfile));
+
     let option = glfw.create_window(400, 300, "Hello World", glfw::Windowed);
 
     match option {
@@ -18,15 +28,28 @@ fn main() {
             window.set_key_polling(true);
             window.make_current();
 
+            gl::load_with( |s| glfw.get_proc_address(s) );
+            glfw.set_swap_interval(1);
+
             while !window.should_close() {
                 glfw.poll_events();
+
+                clear();
+
                 for (_, event) in glfw::flush_messages(&events) {
                     handle_window_event(&window, event);
                 }
+
+                window.swap_buffers();
             }
         },
         None => {}
     }
+}
+
+fn clear() {
+    gl::ClearColor(0.3, 0.3, 0.3, 1.0);
+    gl::Clear(gl::COLOR_BUFFER_BIT);
 }
 
 fn handle_window_event(window: &glfw::Window, event: glfw::WindowEvent) {
